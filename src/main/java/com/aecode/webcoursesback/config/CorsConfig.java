0259103bf808789c0356f968fa05f57a49aeda39
ManuelAecode <teamdev.genplus@gmail.com>
2024-09-03 11:20:21 -0500

    update CORS

--- src/main/java/com/aecode/webcoursesback/config/CorsConfig.java
+++ src/main/java/com/aecode/webcoursesback/config/CorsConfig.java
@@ -1,10 +1,12 @@
 package com.aecode.webcoursesback.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Value("${file.upload-dir}")
     private String uploadDir;
